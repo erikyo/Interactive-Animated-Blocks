31a6f02b9e9e7c67fe3629855d0d975f79e18ce2
Erik Golinelli <erik@codekraft.it>
2022-07-12 12:26:44 +0200

    compress wordpress videos with ffmpeg

--- ssc.php
+++ ssc.php
@@ -7,40 +7,40 @@
  * Author: codekraft
  */
 function ssc_editor_scripts() {
-	$asset = include __DIR__ .'/build/ssc-editor.asset.php';
-	wp_enqueue_script(
-		'ssc-gutenberg',
-		plugin_dir_url( __FILE__ ) . 'build/ssc-editor.js',
-		$asset['dependencies']
-	);
+	$asset = include __DIR__ . '/build/ssc-editor.asset.php';
+	wp_enqueue_script( 'ssc-gutenberg', plugin_dir_url( __FILE__ ) . 'build/ssc-editor.js', $asset['dependencies'] );
 }
+
 add_action( 'enqueue_block_editor_assets', 'ssc_editor_scripts' );
 
 
 function ssc_script() {
 	$asset = include __DIR__ . '/build/ssc.asset.php';
-	wp_enqueue_script(
-		'ssc',
-		plugin_dir_url( __FILE__ ) . 'build/ssc.js',
-		$asset['dependencies']
-	);
+	wp_enqueue_script( 'ssc', plugin_dir_url( __FILE__ ) . 'build/ssc.js', $asset['dependencies'] );
 }
+
 add_action( 'wp_enqueue_scripts', 'ssc_script' );
 
 /**
  * Register and enqueue a custom stylesheet in the WordPress admin.
  */
 function ssc_editor_style() {
-	wp_register_style( 'ssc_admin_css', plugin_dir_url( __FILE__ ) . 'build/ssc-editor.css');
+	wp_register_style( 'ssc_admin_css', plugin_dir_url( __FILE__ ) . 'build/ssc-editor.css' );
 	wp_enqueue_style( 'ssc_admin_css' );
 }
+
 add_action( 'admin_enqueue_scripts', 'ssc_editor_style' );
 
 /**
  * Register and enqueue the ssc stylesheet
  */
 function ssc_style() {
-	wp_register_style( 'ssc_css', plugin_dir_url( __FILE__ ) . 'build/ssc.css');
+	wp_register_style( 'ssc_css', plugin_dir_url( __FILE__ ) . 'build/ssc.css' );
 	wp_enqueue_style( 'ssc_css' );
 }
+
 add_action( 'wp_enqueue_scripts', 'ssc_style' );
+
+// TOOLS
+// filter the video upload and create a copy of that image
+include_once __DIR__ . '/tools/video_upload.php';
