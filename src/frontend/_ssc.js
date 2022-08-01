2eee996dbdd9b2c7ff88e9825afcb5aaeb6d71a7
Erik Golinelli <erik@codekraft.it>
2022-08-01 18:54:09 +0200

    visibility functions enhancement

--- src/frontend/_ssc.js
+++ src/frontend/_ssc.js
@@ -4,10 +4,9 @@ import { getElelementData } from '../utils/fn';
 import anime from 'animejs';
 import ScrollMagic from 'scrollmagic';
 import { ScrollMagicPluginIndicator } from 'scrollmagic-plugins';
+import { sscOptions } from '../ssc';
 
 ScrollMagicPluginIndicator( ScrollMagic );
-
-import { sscOptions } from '../ssc';
 
 // on load and on hashchange (usually on history back/forward)
 const jumpToHash = () => {
@@ -19,6 +18,101 @@ const jumpToHash = () => {
 window.addEventListener( 'load', jumpToHash );
 window.addEventListener( 'hashchange', jumpToHash );
 
+/**
+ * Checks when the element is inside the viewport
+ *
+ * If the top of the element is above the bottom of the viewport and the bottom of the element is below the top of the viewport, then the element is partially visible.
+ *
+ * @param {HTMLElement} el - The element you want to check.
+ * @return {boolean} true when the element is shown inside the viewport
+ */
+export const isPartiallyVisible = ( el ) => {
+	const rect = el.getBoundingClientRect();
+	return rect.top < window.innerHeight && rect.bottom > 0;
+};
+
+/**
+ * Checks when the element is completely inside in the viewport edges
+ *
+ * "If the top of the element is greater than or equal to 0 and the bottom of the element is less than or equal to the height of the window, then the element is fully visible."
+ *
+ * @param {HTMLElement} el - The element you want to check if it's fully visible.
+ * @return {boolean} if the item edges are completely inside the viewport
+ */
+export const isFullyVisible = ( el ) => {
+	const rect = el.getBoundingClientRect();
+	return rect.top >= 0 && rect.bottom <= window.innerHeight;
+};
+
+/**
+ * Check when the element is inside the active area
+ *
+ * "Is the element's center within 20% of the viewport's height from the top and bottom?"
+ * The function returns true if the element's center is within the range, and false if it's not
+ *
+ * @param {HTMLElement} el            - The element you want to check if it's in the active area.
+ * @param {number}      rangePosition - The percentage of the viewport's height to use as active area
+ * @return {boolean}                  - true if the element is inside the active area
+ */
+export const isActiveArea = ( el, rangePosition ) => {
+	const rect = el.getBoundingClientRect();
+	const limit = window.innerHeight * ( rangePosition * 0.005 ); // 20% of 1000px is 100px from top and 100px from bottom
+	const elementCenter = rect.top + rect.height * 0.5;
+	return elementCenter > limit && elementCenter < window.innerHeight - limit;
+};
+
+/**
+ * Check if the element is above or below a certain percentage of the screen
+ *
+ * @param {HTMLElement} el            - The element you want to check if it's crossing the center of the screen.
+ * @param {number}      rangePosition - The percentage of the viewport height that the element should be at.
+ * @return {boolean}                     - return true if above, false if below
+ */
+export const isCrossing = ( el, rangePosition ) => {
+	const rect = el.getBoundingClientRect();
+	const center = parseInt(
+		window.innerHeight * ( rangePosition * 0.01 ),
+		10
+	);
+	return rect.top > center && rect.bottom < center;
+};
+
+/**
+ * If the center of the element is between the top and bottom margins of the active area, then the element is in view
+ *
+ * @param {Object} position         - The stored original position of the element in the viewport.
+ * @param {number} intersectionArea - The percentage of the element that needs to be visible in order to trigger the animation.
+ * @return {boolean}                - the element is in view true / false
+ */
+export const isInView = ( position, intersectionArea ) => {
+	position.ycenter = ( position.yTop + position.yBottom ) * 0.5;
+	const activeArea = intersectionArea * ( window.innerHeight * 0.01 );
+	const inactiveArea = ( window.innerHeight - activeArea ) * 0.5;
+	const margins = {
+		top: window.scrollY + inactiveArea,
+		bottom: window.scrollY + ( window.innerHeight - inactiveArea ),
+	};
+	return margins.top < position.ycenter && margins.bottom > position.ycenter;
+};
+
+/**
+ * If the mouse is over the element, return true.
+ *
+ * @param {Event} e - The event object
+ * @return {boolean} - Return true when the given element has the mouse over
+ */
+export const hasMouseOver = ( e ) => {
+	const mouseX = e;
+	const mouseY = e;
+	const rect = e.target.getBoundingClientRect();
+
+	return rect.left < mouseX < rect.right && rect.top < mouseY < rect.bottom;
+};
+
+/**
+ * @class _ssc
+ *
+ */
 export default class _ssc {
 	constructor( options ) {
 		this.page = options.page || document.body;
@@ -100,7 +194,7 @@ export default class _ssc {
 			.then( () => {
 				this.windowData = {
 					viewHeight: window.innerHeight,
-					lastScrollPosition: window.pageYOffset,
+					lastScrollPosition: window.scrollY,
 				};
 				console.warn( 'New Screensize', this.windowData );
 			} );
@@ -113,10 +207,8 @@ export default class _ssc {
 				return this.isPartiallyVisible( rect );
 			case 'visible':
 				return this.isFullyVisible( rect );
-			case 'crossing':
-				return this.isCrossing( rect, position );
-			case 'between':
-				return this.isBetween( rect, position );
+			case 'inActiveArea':
+				return this.isActiveArea( rect, position );
 		}
 	}
 
@@ -128,18 +220,35 @@ export default class _ssc {
 		return rect.top >= 0 && rect.bottom <= this.windowData.viewHeight;
 	}
 
-	isCrossing( rect, rangePosition ) {
-		const center = this.windowData.viewHeight * ( rangePosition * 0.01 );
-		return rect.top < center && rect.bottom > center;
-	}
-
-	isBetween( rect, rangePosition ) {
-		const limit = this.windowData.viewHeight * ( rangePosition * 0.005 ); // 20% of 1000px is 100px from top and 100px from bottom
+	isActiveArea( rect, rangePosition ) {
+		const limit = parseInt(
+			this.windowData.viewHeight * ( rangePosition * 0.005 ),
+			10
+		); // 20% of 1000px is 100px from top and 100px from bottom
 		const elementCenter = rect.top + rect.height * 0.5;
 		return (
 			elementCenter > limit &&
 			elementCenter < this.windowData.viewHeight - limit
 		);
+	}
+
+	isCrossing( rect, rangePosition ) {
+		const center = parseInt(
+			this.windowData.viewHeight * ( rangePosition * 0.01 ),
+			10
+		);
+		return rect.top < center && rect.bottom > center;
+	}
+
+	isBetween( el, intersectionArea ) {
+		const elCenter = ( el.position.yTop + el.position.yBottom ) * 0.5;
+		const activeArea = intersectionArea * ( window.innerHeight * 0.01 );
+		const inactiveArea = ( window.innerHeight - activeArea ) * 0.5;
+		const margins = {
+			top: window.scrollY + inactiveArea,
+			bottom: window.scrollY + ( window.innerHeight - inactiveArea ),
+		};
+		return margins.top > elCenter && elCenter < margins.bottom;
 	}
 
 	hasMouseOver( e ) {
@@ -158,9 +267,9 @@ export default class _ssc {
 	};
 
 	scrollDirection() {
-		if ( this.windowData.lastScrollPosition < window.pageYOffset ) {
+		if ( this.windowData.lastScrollPosition < window.scrollY ) {
 			document.body.dataset.direction = 'down';
-		} else if ( this.windowData.lastScrollPosition > window.pageYOffset ) {
+		} else if ( this.windowData.lastScrollPosition > window.scrollY ) {
 			document.body.dataset.direction = 'up';
 		}
 	}
@@ -173,7 +282,7 @@ export default class _ssc {
 			this.parallaxed.length
 		) {
 			// if last position is the same as current
-			if ( window.pageYOffset === this.windowData.lastScrollPosition ) {
+			if ( window.scrollY === this.windowData.lastScrollPosition ) {
 				// callback the animationFrame and exit the current loop
 				window.requestAnimationFrame( this.parallax );
 				return;
@@ -201,7 +310,7 @@ export default class _ssc {
 				window.requestAnimationFrame( this.parallax );
 
 				// Store the last position
-				this.windowData.lastScrollPosition = window.pageYOffset;
+				this.windowData.lastScrollPosition = window.scrollY;
 			} );
 		}
 		return false;
@@ -493,7 +602,7 @@ export default class _ssc {
 		} );
 
 		// store the last scroll position
-		this.windowData.lastScrollPosition = window.pageYOffset;
+		this.windowData.lastScrollPosition = window.scrollY;
 	};
 
 	// handleRefreshInterval() {}
