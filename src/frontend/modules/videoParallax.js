9f6ad2a9fd0cf01c71226f5d1120d6f632c74cea
Erik Golinelli <erik@codekraft.it>
2022-08-04 12:51:06 +0200

    videoParallax.js module - the video scroll parallax animation (playback is "linked" to window scrollY)

---
+++ src/frontend/modules/videoParallax.js
@@ -0,0 +1,86 @@
+import { isPartiallyVisible } from '../../utils/utils';
+
+let videoParallaxed = [];
+let lastVideoScrollPosition = 0;
+
+export function parallaxVideo() {
+	if ( window.scrollY === lastVideoScrollPosition ) {
+		// callback the animationFrame and exit the current loop
+		return window.requestAnimationFrame( parallaxVideo );
+	}
+
+	// Store the last position
+	lastVideoScrollPosition = window.scrollY;
+
+	videoParallaxed.forEach( ( video ) => {
+		const rect = video.item.getBoundingClientRect();
+		if ( video.hasExtraTimeline ) {
+			console.log(
+				'scrolling timeline',
+				( window.scrollY -
+					video.distanceTop +
+					rect.top +
+					rect.height ) /
+					video.hasExtraTimeline,
+				'regular timeline',
+				1 - ( rect.top + rect.height ) / video.hasExtraTimeline
+			);
+			// the common behaviour
+			video.item.currentTime = (
+				( ( window.scrollY - video.distanceTop ) /
+					video.hasExtraTimeline +
+					( 1 -
+						( rect.top + rect.height ) /
+							video.hasExtraTimeline ) ) *
+				video.videoDuration *
+				video.playbackRatio
+			).toFixed( 5 );
+		} else {
+			// the common behaviour
+			video.item.currentTime = (
+				( 1 - ( rect.top + rect.height ) / video.timelineLength ) *
+				video.videoDuration *
+				video.playbackRatio
+			).toFixed( 5 );
+		}
+	} );
+
+	return window.requestAnimationFrame( parallaxVideo );
+}
+
+function videoParallaxController( entry ) {
+	const videoEl = entry.target.querySelector( 'video' );
+	if ( videoEl && ! videoParallaxed[ entry.target.sscItemData.sscItem ] ) {
+		if ( isPartiallyVisible( videoEl ) ) {
+			const rect = entry.target.getBoundingClientRect();
+			let timelineDuration =
+				parseInt( entry.target.sscItemData.timelineDuration, 10 ) || 0;
+			timelineDuration =
+				entry.target.sscItemData.intersection === 'down'
+					? timelineDuration
+					: timelineDuration * -1;
+			const duration =
+				rect.height + window.innerHeight + timelineDuration;
+			videoParallaxed[ entry.target.sscItemData.sscItem ] = {
+				item: videoEl,
+				videoDuration: videoEl.duration,
+				sscItemData: entry.target.sscItemData,
+				hasExtraTimeline: timelineDuration,
+				timelineLength: duration,
+				distanceTop: rect.height + rect.top + window.scrollY,
+				playbackRatio: parseFloat(
+					entry.target.sscItemOpts.playbackRatio
+				).toFixed( 2 ),
+			};
+		}
+		parallaxVideo();
+	}
+
+	if ( entry.target.action === 'leave' ) {
+		return ( videoParallaxed = videoParallaxed.filter(
+			( item ) =>
+				item.sscItemData.sscItem !== entry.target.sscItemData.sscItem
+		) );
+	}
+}
+export default videoParallaxController;
