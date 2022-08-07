a4f256641200436ce386fa99fa11be0fbbf053a8
Erik Golinelli <erik@codekraft.it>
2022-08-07 16:30:11 +0200

    shows custom css inside the block editor

--- src/admin/editAttributes.js
+++ src/admin/editAttributes.js
@@ -1,31 +1,48 @@
 import { createHigherOrderComponent } from '@wordpress/compose';
 import classnames from 'classnames';
 
+/**
+ * It's a higher order component that adds the custom classes into block editor
+ */
 export const editAttributes = createHigherOrderComponent(
 	( BlockListBlock ) => {
 		return ( props, block ) => {
 			const {
-				attributes: { additionalClasses },
+				attributes: { additionalClasses, additionalCSS },
 				className,
 			} = props;
 
-			const customClasses = [];
+			if ( additionalClasses || additionalCSS ) {
+				const customClasses = [];
+				const additional = {
+					style: {},
+				};
 
-			Object.entries( additionalClasses ).map( ( cssClass ) =>
-				cssClass[ 1 ] !== false
-					? customClasses.push( 'ssc-' + cssClass[ 0 ] )
-					: null
-			);
+				Object.entries( additionalClasses ).map( ( cssClass ) =>
+					cssClass[ 1 ] !== false
+						? customClasses.push( 'ssc-' + cssClass[ 0 ] )
+						: null
+				);
 
-			return (
-				<BlockListBlock
-					{ ...block }
-					{ ...props }
-					className={
-						classnames( className, customClasses.join( ' ' ) ) || ''
-					}
-				/>
-			);
+				Object.entries( additionalCSS ).map( ( cssStyle ) =>
+					cssStyle[ 1 ] !== false
+						? additional.style[ cssStyle[ 0 ] ] = cssStyle[ 1 ]
+						: null
+				);
+
+				return (
+					<BlockListBlock
+						{ ...block }
+						{ ...props }
+						className={
+							classnames( className, customClasses.join( ' ' ) ) || ''
+						}
+						wrapperProps={ { style: { ...additionalCSS } } }
+					/>
+				);
+			}
+
+			return <BlockListBlock { ...props } />;
 		};
 	},
 	'withClientIdClassName'
