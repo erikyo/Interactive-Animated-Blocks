30e1e5524129c363d26373d893a679f6826a59f9
Erik Golinelli <erik@codekraft.it>
2022-09-06 18:24:03 +0200

    extends the default limits of the intersection observer so that jackscrolling is not triggered when not wanted
    Took 2 minutes

--- src/ssc.js
+++ src/ssc.js
@@ -1,16 +1,25 @@
 import './styles/ssc.scss';
 import _ssc from './frontend/_ssc';
 
+
 /**
- * Exporting the sscOptions object.
+ * Exports the sscOptions object.
+ * It returns an object with the rootMargin, threshold, and container properties
+ *
+ * @returns An object with three properties:
+ * rootMargin: A string that defines the margins around the root element.
+ * threshold: An array of numbers between 0 and 1 that defines the percentage of the target element that should be visible before it is considered visible.
+ * container: The element that is used as the viewport for checking visibility of the target.
  */
-export const sscOptions = {
-	rootMargin: '0px', // the IntersectionObserver root margin
-	// removed because we only need to know if the element is entering or leaving the viewport
-	// const intersectionPrecision = 5;
-	// threshold: [ ...Array( intersectionPrecision + 1 ).keys() ].map( ( x ) => x / intersectionPrecision ), // 1-100 the precision of intersections (higher number increase cpu usage - use with care!)
-	threshold: [ 0 ],
-	container: document.querySelector( '.wp-site-blocks' ), // maybe document.getElementById( 'page' );
+export const sscOptions = function() {
+	return {
+		rootMargin: document.body.classList.contains( 'admin-bar' ) ? '32px 0px 1px 0px' : '1px', // the IntersectionObserver root margin
+		// removed because we only need to know if the element is entering or leaving the viewport
+		// const intersectionPrecision = 5;
+		// threshold: [ ...Array( intersectionPrecision + 1 ).keys() ].map( ( x ) => x / intersectionPrecision ), // 1-100 the precision of intersections (higher number increase cpu usage - use with care!)
+		threshold: [ 0 ],
+		container: document.querySelector( '.wp-site-blocks' ) || document.body, // maybe document.getElementById( 'page' );
+	};
 };
 
 /**
