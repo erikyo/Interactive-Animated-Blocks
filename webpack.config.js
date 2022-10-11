dd38333496ceee5b2d45f28c787f3836d667987d
Erik Golinelli <erik@codekraft.it>
2022-10-11 12:40:10 +0200

    webpack config
    Took 27 seconds

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,16 @@
 const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
+const path = require( 'path' );
+
+const entry = {};
+[ 'ssc', 'ssc-editor' ].forEach(
+	( script ) =>
+		( entry[ script ] = path.resolve(
+			process.cwd(),
+			`src/${ script }.js`
+		) )
+);
 
 module.exports = {
 	...defaultConfig,
+	entry,
 };
