5a257bacb56432c2cf1024baa9a3502965f3e5fe
Erik Golinelli <erik@codekraft.it>
2022-10-11 12:59:43 +0200

    updated dependencies, doc && build
    Took 2 minutes

--- build/ssc-editor.asset.php
+++ build/ssc-editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '9e05cef037f5fbb36d6e');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '88e081b6ef57be3a9c8b');
--- build/ssc.asset.php
+++ build/ssc.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '7532a93f7607356198d9');
+<?php return array('dependencies' => array(), 'version' => 'd0244e1153d95a0ea6fe');
