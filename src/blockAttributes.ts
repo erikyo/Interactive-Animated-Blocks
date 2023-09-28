0e04947ebccf5ed55c6cb24a295cb01b63cec0b5
Erik Golinelli <erik@codekraft.it>
2023-09-28 12:02:55 +0200

    updated additional panel settings

--- src/blockAttributes.ts
+++ src/blockAttributes.ts
@@ -1,23 +1,20 @@
-import { BlockAttributes } from '@wordpress/blocks';
-
 /**
  * The ssc additional panel settings.
  *
- * @param {Object} settings            Settings for the block.
- *
- * @param          settings.attributes
+ * @param {Object} settings            the settings object for the block.
+ * @param          settings.attributes the attributes object for the block.
  * @return {Object} settings Modified settings.
  */
-export function addAttributes( settings: BlockAttributes ) {
+export function addAttributes(settings: { attributes: any }) {
 	//check if object exists for old Gutenberg version compatibility
 	//add allowedBlocks restriction
-	if ( typeof settings.attributes !== 'undefined' ) {
-		settings.attributes = Object.assign( settings.attributes, {
+	if (typeof settings.attributes !== 'undefined') {
+		settings.attributes = Object.assign(settings.attributes, {
 			ssc: {
 				type: 'object',
 				default: {},
 			},
-		} );
+		});
 	}
 
 	return settings;
