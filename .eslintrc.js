58a457aabb51513a73c5af2e16c1744d8b2ce4bb
Erik Golinelli <erik@codekraft.it>
2023-09-28 18:09:31 +0200

    env

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,26 +3,12 @@
  */
 
 const eslintConfig = {
-	extends: [ 'plugin:@wordpress/eslint-plugin/recommended' ],
+	extends: ['plugin:@wordpress/eslint-plugin/recommended'],
 };
 
 eslintConfig.parserOptions = {
-	ecmaVersion: 6,
-	env: {
-    jest: true,
-    es6: true
-  },
 	babelOptions: {
-		presets: [
-			'@wordpress/babel-preset-default',
-			'@babel/preset-typescript',
-		],
-    globals: {
-      page: true,
-      browser: true,
-      context: true,
-      jestPuppeteer: true,
-    },
+		presets: ['@wordpress/babel-preset-default'],
 	},
 };
 
