376e660e0a4138e2ec110b443682d69c81c6d8b8
Erik Golinelli <erik@codekraft.it>
2023-09-28 11:55:10 +0200

    removes the build folder

--- build/ssc-editor.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => 'eeb349a2ae748238bc0a');
--- build/ssc.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => 'e8fb45344791f53d14a7');
