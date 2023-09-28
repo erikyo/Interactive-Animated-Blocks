ad697e61da4a2f136a5fdf0b5326734da4e95047
Erik Golinelli <erik@codekraft.it>
2023-09-28 12:41:19 +0200

    updated init

--- src/ssc.ts
+++ src/ssc.ts
@@ -1,19 +1,20 @@
 import './styles/ssc.scss';
 import _ssc from './frontend/_ssc';
 import { sscOptionsDefault } from './frontend/constants';
+import { SscOptions } from './types';
 
 /**
  * this is for further customization of the default options
  */
-export const options = { ...sscOptionsDefault };
+export const options: SscOptions = { ...sscOptionsDefault };
 
 /**
  * On page scripts load trigger immediately ssc using sscOptions
  */
-window.addEventListener( 'load', () => {
-	if ( typeof window.screenControl ) {
-		window.screenControl = new _ssc( options );
+window.addEventListener('load', () => {
+	if (typeof window.screenControl) {
+		window.screenControl = new _ssc(options);
 	} else {
-		throw new Error( 'SSC ERROR: multiple instances loaded' );
+		throw new Error('SSC ERROR: multiple instances loaded');
 	}
-} );
+});
