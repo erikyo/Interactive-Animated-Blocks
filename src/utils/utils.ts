f7a1a2b0e50001ecb579dfad12abdd977f1f0299
Erik Golinelli <erik@codekraft.it>
2023-09-29 11:36:28 +0200

    updates the isInView function

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,5 +1,5 @@
 import { windowProps } from '../frontend/_ssc';
-import type {SscPositionYDef, WindowProps} from '../types.d.ts';
+import type { SscPositionYDef, WindowProps } from '../types.d.ts';
 
 /**
  * This function returns a promise that resolves after the given number of milliseconds.
@@ -109,14 +109,19 @@ export const isInView = (
 	position: SscPositionYDef,
 	intersectionArea: number
 ) => {
-	position.ycenter = (position.yTop + position.yBottom) * 0.5;
+	// return false if the element has no position data
+	if (!position?.yTop || !position?.yBottom) return false;
+	// get the center of the element
+	const ycenter = (position.yTop + position.yBottom) * 0.5;
+	// calculate the margins of the active area
 	const activeArea = intersectionArea * (window.innerHeight * 0.01);
 	const inactiveArea = (window.innerHeight - activeArea) * 0.5;
 	const margins = {
 		top: window.scrollY + inactiveArea,
 		bottom: window.scrollY + (window.innerHeight - inactiveArea),
 	};
-	return margins.top < position.ycenter && margins.bottom > position.ycenter;
+	// return true if the center of the element is between the top and bottom margins of the active area
+	return margins.top < ycenter && margins.bottom > ycenter;
 };
 
 /**
